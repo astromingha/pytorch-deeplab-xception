aa6e6e7f7df12cd7895e06e955621ac7ea1da730
astromingha <innopam.mh@gmail.com>
2020-11-05 17:40:52 +0900

    adding masking result images fcn

--- demo_release.py
+++ demo_release.py
@@ -1,7 +1,7 @@
 from modeling.inference_main import Inference
 import cv2
 import os
-
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 def main():
 
     ## model load
@@ -9,7 +9,8 @@ def main():
 
     ## image load (http method로 대체)
     image_path = 'config/radish_1-1_15.jpg'
-    img_dir = '/home/user/Dataset/JejuAI/radish/cityscape_format/2nd/leftImg8bit/val'
+    img_dir = '/home/user/Desktop/png_shards'#image_valid'#png_shards'
+    out_dir = '/home/user/Desktop/masked_images_v2'
     for img in os.listdir(img_dir):
         image_path = os.path.join(img_dir,img)
 
@@ -17,18 +18,16 @@ def main():
         img_ndarray = cv2.cvtColor(img_ndarray, cv2.COLOR_RGB2BGR)
 
         ## inference output -> segmentations = [[x1,y1,x2,y2..],[x1,y1,x2,y2..],..]
-        segmentation, contours = inference(img_ndarray)
+        # segmentation, contours = inference(img_ndarray)
+        segmentation = inference(img_ndarray)
 
 
         ## Geo-referencing ##
 
+        # ########## inference test code (visualization) ##########
+        # inference.drawContour(os.path.join(out_dir, img))
+        inference.maskImg(os.path.join(out_dir,img))
 
-
-        ########## inference test code (visualization) ##########
-        img_ndarray = cv2.cvtColor(img_ndarray, cv2.COLOR_BGR2RGB)
-        for cnt in contours:
-            cv2.drawContours(img_ndarray,[cnt],0,(255,255,255),3)
-        cv2.imwrite('/home/user/Desktop/radish/'+img,img_ndarray)
 
 
 
